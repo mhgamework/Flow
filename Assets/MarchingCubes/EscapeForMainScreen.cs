6185b39aab6263d1d028f0b3c89a495a5a3cde7c
Michiel Huygen <mhgamework@gmail;com>
2017-09-10 13:45:44 +0200

    Some cleanup

---
+++ Assets/MarchingCubes/EscapeForMainScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeForMainScreen : MonoBehaviour {
+    public string SceneName;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+	        SceneManager.LoadScene(SceneName);
+        }
+    }
+}
---
+++ Assets/MarchingCubes/GoToScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToScene : MonoBehaviour
+{
+    public string SceneName;
+    // Use this for initialization
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(onClick);
+    }
+
+    private void onClick()
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
