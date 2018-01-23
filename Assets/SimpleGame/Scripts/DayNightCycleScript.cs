cbe39e366c011c92d315b985273c69cbdfde47e8
Michiel Huygen <mhgamework@gmail;com>
2018-01-23 09:39:46 +0100

    Simple day-night effect

---
+++ Assets/SimpleGame/Scripts/DayNightCycleScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.SimpleGame.Scripts
+{
+    public class DayNightCycleScript : MonoBehaviour
+    {
+        public Vector3 RotationAxis;
+        public float DayDuration;
+        public void Start()
+        {
+            
+        }
+        public void Update()
+        {
+            transform.Rotate(RotationAxis, 360 / DayDuration * Time.deltaTime);
+        }
+    }
+}
