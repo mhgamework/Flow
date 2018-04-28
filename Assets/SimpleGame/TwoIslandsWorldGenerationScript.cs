72c8e7cbba5d4c3747347dffeb55194749ced4fa
Michiel Huygen <mhgamework@gmail;com>
2018-04-28 21:11:27 +0200

    Island with noise

--- Assets/SimpleGame/TwoIslandsWorldGenerationScript.cs
+++ Assets/SimpleGame/TwoIslandsWorldGenerationScript.cs
@@ -47,18 +47,27 @@ namespace Assets.MarchingCubes.Scenes
         {
 
             //var scale = 1f/25;
-            //var scale = 1f/10;
-            var scale = 1f/1;
+            //var scale = 1f / 10;
+            //var scale = 1f / 4;
+            var scale = 1f / 2;
+            //var scale = 1f / 1;
 
-            var vector3 = new Vector3(100,100,100)*scale;
+            var vector3 = new Vector3(100, 100, 100) * scale;
 
             float island1Size;
             Vector3 island1Pos;
 
             island1Size = 100f * scale;
-            island1Pos = vector3 + new Vector3(island1Size, island1Size, island1Size)*2;
+            island1Pos = vector3 + new Vector3(island1Size, island1Size, island1Size) * 2;
 
-            addIsland(island1Pos, island1Size, vector3, scale);
+            Vector3 renderCenter = island1Pos;
+            float renderRange = island1Size * 4;
+
+            //renderCenter += Vector3.one * island1Size * 0.5f;
+            //renderRange = island1Size * 1f;
+
+            renderRange = island1Size * 0.2f;
+            addIsland(island1Pos, island1Size, vector3, scale, renderCenter - Vector3.one * renderRange, renderCenter + Vector3.one * renderRange);
 
             //island1Size = 100f * scale*0.5f;
             //island1Pos = vector3 + new Vector3(island1Size, island1Size, island1Size)*2;
@@ -93,7 +102,7 @@ namespace Assets.MarchingCubes.Scenes
 
         }
 
-        private void addIsland(Vector3 island1Pos, float island1Size, Vector3 vector3, float scale)
+        private void addIsland(Vector3 island1Pos, float island1Size, Vector3 vector3, float scale, Vector3 boundMin, Vector3 boundMax)
         {
             var island1 = new Ball(island1Pos, island1Size);
 
@@ -105,18 +114,19 @@ namespace Assets.MarchingCubes.Scenes
             perlin.SetSeed(0);
             var mat = new VoxelMaterial(Color.gray);
             var size = new Vector3(1, 1, 1) * 32;
-            world.RunKernel1by1((island1Pos - Vector3.one * island1Size * 4).ToFloored(),
-                (island1Pos + Vector3.one * island1Size *4).ToCeiled(), (v, p) =>
+            world.RunKernel1by1(boundMin.ToFloored(),
+                boundMax.ToCeiled(), (v, p) =>
                 {
                     var realP = p.ToVector3();
-                    realP.y = (realP.y - island1Pos.y)/2f + island1Pos.y;
+                    realP.y = (realP.y - island1Pos.y) / 2f + island1Pos.y;
 
                     var coords = realP * 0.03f / scale;
-                    
+
                     v.Density = island1.Sdf(realP) + Mathf.Max(-(realP.y - (island1Pos.y)), 0);// + Mathf.Max((p.Y - (island1Pos.y + 10)), 0)*0.3f;
 
-                    v.Density = Mathf.Max(v.Density,  (realP.y - (island1Pos.y)));
-                    //v.Density += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f;
+                    v.Density = Mathf.Max(v.Density, (realP.y - (island1Pos.y)));
+                    v.Density += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f;
+                    v.Density += perlin.Noise(coords.x*3.1f, coords.y * 3.1f, coords.z * 3.1f) * island1Size * 0.1f;
                     v.Material = mat;
                     return v;
                 }, 123);
@@ -133,9 +143,9 @@ namespace Assets.MarchingCubes.Scenes
 
             for (int i = 0; i < 5; i++)
             {
-                var sphere = new Ball(vector3 + new Vector3(30, 30, 30)*i, 20);
+                var sphere = new Ball(vector3 + new Vector3(30, 30, 30) * i, 20);
 
-                editingService.AddSDFObject(world, sphere, new Bounds(vector3 + new Vector3(30, 30, 30)*i, new Vector3(1, 1, 1) * 20 * 2 * 1.5f), new VoxelMaterial(Color.red), 20);
+                editingService.AddSDFObject(world, sphere, new Bounds(vector3 + new Vector3(30, 30, 30) * i, new Vector3(1, 1, 1) * 20 * 2 * 1.5f), new VoxelMaterial(Color.red), 20);
 
             }
 
@@ -147,10 +157,10 @@ namespace Assets.MarchingCubes.Scenes
             perlin.SetSeed(0);
             var mat = new VoxelMaterial(Color.gray);
             var size = new Vector3(1, 1, 1) * 32;
-            world.RunKernel1by1(vector3.ToFloored(), (vector3 + size).ToCeiled(), (v,p) =>
+            world.RunKernel1by1(vector3.ToFloored(), (vector3 + size).ToCeiled(), (v, p) =>
             {
                 var coords = p.ToVector3() * 0.11f;
-                v.Density = perlin.Noise(coords.x,coords.y,coords.z);
+                v.Density = perlin.Noise(coords.x, coords.y, coords.z);
                 v.Material = mat;
                 return v;
             }, 123);
@@ -162,20 +172,20 @@ namespace Assets.MarchingCubes.Scenes
             bool isEmpty;
 
             var world = (OctreeVoxelWorld)this.world;
-            var mapData = new MapData(world.ChunkSize.X+2);
+            var mapData = new MapData(world.ChunkSize.X + 2);
 
             var helper = new ClipMapsOctree<OctreeNode>();
-            
-            helper.VisitTopDown(world.GetNode(new DirectX11.Point3(world.getNodeResolution(8)*world.ChunkSize.X*3, 0, 0), 8), n =>
-            {
-                n = world.GetNode(n.LowerLeft, n.Depth);
-                world.makeUnsharedChunk(n);
-                tempVoxelWorldGenerator.GenerateMapData(mapData, n.LowerLeft.ToVector3().TakeXZ(),
-                    world.getNodeResolution(n.Depth), world.ChunkSize.X + 2, 0.1f);
 
-                tempVoxelWorldGenerator.GenerateVoxelData(mapData, world.getNodeResolution(n.Depth),n.LowerLeft.Y,
-                    tempVoxelWorldGenerator.GetMaterialsDictionary(), 0.1f, n.VoxelData.Data, out isEmpty);
-            });
+            helper.VisitTopDown(world.GetNode(new DirectX11.Point3(world.getNodeResolution(8) * world.ChunkSize.X * 3, 0, 0), 8), n =>
+                {
+                    n = world.GetNode(n.LowerLeft, n.Depth);
+                    world.makeUnsharedChunk(n);
+                    tempVoxelWorldGenerator.GenerateMapData(mapData, n.LowerLeft.ToVector3().TakeXZ(),
+                        world.getNodeResolution(n.Depth), world.ChunkSize.X + 2, 0.1f);
+
+                    tempVoxelWorldGenerator.GenerateVoxelData(mapData, world.getNodeResolution(n.Depth), n.LowerLeft.Y,
+                        tempVoxelWorldGenerator.GetMaterialsDictionary(), 0.1f, n.VoxelData.Data, out isEmpty);
+                });
 
 
 
@@ -183,7 +193,7 @@ namespace Assets.MarchingCubes.Scenes
 
         private void createSDFWithNoise(Vector3 vector3)
         {
-            var s2 = new Ball(vector3 + new Vector3(1, 1, 1)*16, 11);
+            var s2 = new Ball(vector3 + new Vector3(1, 1, 1) * 16, 11);
             var perlin = new TreeEditor.Perlin();
             perlin.SetSeed(0);
             var mat = new VoxelMaterial(Color.gray);
@@ -191,7 +201,7 @@ namespace Assets.MarchingCubes.Scenes
             world.RunKernel1by1(vector3.ToFloored(), (vector3 + size).ToCeiled(), (v, p) =>
             {
                 var coords = p.ToVector3() * 0.33f;
-                v.Density = perlin.Noise(coords.x, coords.y, coords.z)*3;
+                v.Density = perlin.Noise(coords.x, coords.y, coords.z) * 3;
                 v.Density += s2.Sdf(p);
                 v.Material = mat;
                 return v;
@@ -217,7 +227,7 @@ namespace Assets.MarchingCubes.Scenes
 
 
             Random.InitState(0);
-            
+
             for (int i = 0; i < 10; i++)
             {
                 var offset = Random.onUnitSphere * Random.Range(35, 45);
@@ -225,23 +235,23 @@ namespace Assets.MarchingCubes.Scenes
 
                 s2 = new Ball(vector3 + new Vector3(1, 1, 1) * 32 + offset, range);
                 var center = vector3 + new Vector3(1, 1, 1) * 32 + offset;
-                world.RunKernel1by1((center-Vector3.one*range*1.3f).ToFloored(), (center + Vector3.one * range * 1.3f).ToCeiled(), (v, p) =>
-                {
-                    var coords = p.ToVector3() * 0.5f;
+                world.RunKernel1by1((center - Vector3.one * range * 1.3f).ToFloored(), (center + Vector3.one * range * 1.3f).ToCeiled(), (v, p) =>
+                      {
+                          var coords = p.ToVector3() * 0.5f;
 
-                    var n  = perlin.Noise(coords.x, coords.y, coords.z) * range*0.5f;
-                    n += s2.Sdf(p);
-                    if (n < v.Density) v.Density = n;
+                          var n = perlin.Noise(coords.x, coords.y, coords.z) * range * 0.5f;
+                          n += s2.Sdf(p);
+                          if (n < v.Density) v.Density = n;
 
-                    v.Material = mat;
-                    return v;
-                }, 123);
+                          v.Material = mat;
+                          return v;
+                      }, 123);
             }
 
         }
 
-       
-       
+
+
 
     }
 }
