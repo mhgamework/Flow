7b418f4d4723e9c4a1820e2bcc17dd3e539a1004
Michiel Huygen <mhgamework@gmail;com>
2018-04-29 15:29:08 +0200

    Island with caves

--- Assets/SimpleGame/TwoIslandsWorldGenerationScript.cs
+++ Assets/SimpleGame/TwoIslandsWorldGenerationScript.cs
@@ -49,8 +49,8 @@ namespace Assets.MarchingCubes.Scenes
             //var scale = 1f/25;
             //var scale = 1f / 10;
             //var scale = 1f / 4;
-            var scale = 1f / 2;
-            //var scale = 1f / 1;
+            //var scale = 1f / 2;
+            var scale = 1f / 1;
 
             var vector3 = new Vector3(100, 100, 100) * scale;
 
@@ -66,7 +66,8 @@ namespace Assets.MarchingCubes.Scenes
             //renderCenter += Vector3.one * island1Size * 0.5f;
             //renderRange = island1Size * 1f;
 
-            renderRange = island1Size * 0.2f;
+            renderRange = island1Size * 1.5f;
+
             addIsland(island1Pos, island1Size, vector3, scale, renderCenter - Vector3.one * renderRange, renderCenter + Vector3.one * renderRange);
 
             //island1Size = 100f * scale*0.5f;
@@ -104,6 +105,8 @@ namespace Assets.MarchingCubes.Scenes
 
         private void addIsland(Vector3 island1Pos, float island1Size, Vector3 vector3, float scale, Vector3 boundMin, Vector3 boundMax)
         {
+
+            // More inspiration: http://codeflow.org/entries/2010/dec/09/minecraft-like-rendering-experiments-in-opengl-4/#more-interesting-noise
             var island1 = new Ball(island1Pos, island1Size);
 
             //editingService.AddSDFObject(world, island1, new Bounds(island1Pos, new Vector3(1, 1, 1) * island1Size * 2), new VoxelMaterial(Color.red), 20);
@@ -111,25 +114,103 @@ namespace Assets.MarchingCubes.Scenes
 
             var s2 = new Ball(vector3 + new Vector3(1, 1, 1) * 16, 11);
             var perlin = new TreeEditor.Perlin();
-            perlin.SetSeed(0);
+            perlin.SetSeed(1);
             var mat = new VoxelMaterial(Color.gray);
             var size = new Vector3(1, 1, 1) * 32;
+
+            //var sum = 0.0;
+            var max = 0.0f;
+            var min = 0.0f;
+            //long count = 0;
+
             world.RunKernel1by1(boundMin.ToFloored(),
                 boundMax.ToCeiled(), (v, p) =>
                 {
+                    v.Material = mat;
+
                     var realP = p.ToVector3();
                     realP.y = (realP.y - island1Pos.y) / 2f + island1Pos.y;
 
                     var coords = realP * 0.03f / scale;
 
+                    var caveCoords = p.ToVector3() * 0.03f / scale;
+
+                    var caves = 0f;
+                    caves += Mathf.Pow((noise(perlin, caveCoords * 1.4f) + 0.5f), 3);// * island1Size * 0.1f ;
+                    //caves += perlin.Noise(coords.x*3.1f * 4.1f, coords.y * 3.1f * 4.1f, coords.z * 3.1f * 4.1f) * island1Size * 0.1f ;
+                    //sum += perlin.Noise(caveCoords.x * 3.1f, caveCoords.y * 3.1f, caveCoords.z * 3.1f);
+                    //max = Math.Max(max, noise(perlin,caveCoords * 3.1f));
+                    //min = Math.Min(min, noise(perlin, caveCoords * 3.1f));
+                    //count++;
+                    if (caves > 1 || caves < 0) Debug.Log("kaput");
+                    caves -= 0.3f;
+                    caves = -caves;
+                    caves += Mathf.Clamp(((realP.y - island1Pos.y) - (-island1Size * .1f)) / (0 - (-island1Size * .1f)), 0, 1);
+                    //v.Density = caves;
+
+                    //return v;
+
+
                     v.Density = island1.Sdf(realP) + Mathf.Max(-(realP.y - (island1Pos.y)), 0);// + Mathf.Max((p.Y - (island1Pos.y + 10)), 0)*0.3f;
 
+                    var intensity = Mathf.Lerp(1, 0, Mathf.Max(0, realP.y - island1Pos.y) / (island1Size / 16f));
+
                     v.Density = Mathf.Max(v.Density, (realP.y - (island1Pos.y)));
-                    v.Density += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f;
-                    v.Density += perlin.Noise(coords.x*3.1f, coords.y * 3.1f, coords.z * 3.1f) * island1Size * 0.1f;
-                    v.Material = mat;
+
+                    //v.Density += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f * intensity;
+                    //v.Density += perlin.Noise(coords.x * 3.1f, coords.y * 3.1f, coords.z * 3.1f) * island1Size * 0.1f * intensity;
+
+                    if (-caves > v.Density)
+                    {
+                        v.Density = -caves;
+                        return v;
+                    }
+
+
                     return v;
                 }, 123);
+            //Debug.Log("Count: " + count);
+            //Debug.Log("avg: " + sum/count);
+            Debug.Log("Max: " + max);
+            Debug.Log("Min: " + min);
+            //world.RunKernel1by1(boundMin.ToFloored(),
+            //    boundMax.ToCeiled(), (v, p) =>
+            //    {
+            //        v.Material = mat;
+
+            //        var realP = p.ToVector3();
+            //        realP.y = (realP.y - island1Pos.y) / 2f + island1Pos.y;
+
+            //        var coords = realP * 0.03f / scale;
+
+            //        var caves = 0f;
+            //        caves += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f;
+            //        caves += perlin.Noise(coords.x * 3.1f, coords.y * 3.1f, coords.z * 3.1f) * island1Size * 0.1f;
+
+            //        caves += 0.3f;
+            //        if (caves < 0)
+            //        {
+            //            v.Density = -caves;
+            //            return v;
+            //        }
+            //        v.Density = island1.Sdf(realP) + Mathf.Max(-(realP.y - (island1Pos.y)), 0);// + Mathf.Max((p.Y - (island1Pos.y + 10)), 0)*0.3f;
+
+            //        var intensity = Mathf.Lerp(1, 0, Mathf.Max(0, realP.y - island1Pos.y) / (island1Size / 16f));
+
+            //        //v.Density = Mathf.Max(v.Density, (realP.y - (island1Pos.y)));
+            //        //v.Density += perlin.Noise(coords.x, coords.y, coords.z) * island1Size * 0.3f * intensity;
+            //        //v.Density += perlin.Noise(coords.x * 3.1f, coords.y * 3.1f, coords.z * 3.1f) * island1Size * 0.1f * intensity;
+
+
+
+
+            //        return v;
+            //    }, 234);
+        }
+
+        float noise(TreeEditor.Perlin perlin, Vector3 p)
+        {
+            return perlin.Noise(p.x, p.y, p.z) * (0.5f / 0.6f);
         }
 
         private void createSDFPrimitives(Vector3 vector3)
