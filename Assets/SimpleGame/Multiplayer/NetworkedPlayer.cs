45c04174de1ffac9ff1e2d4df8b4b226529b8bd5
Michiel Huygen <michiel@getsleek.co>
2018-07-10 22:04:00 +0200

    Simple ethan multiplayer

---
+++ Assets/SimpleGame/Multiplayer/NetworkedPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+using Object = System.Object;
+
+namespace Assets.SimpleGame.Multiplayer
+{
+    public class NetworkedPlayer : NetworkBehaviour
+    {
+        public void Start()
+        {
+            Debug.Log("Start networked palyer");
+        }
+        public override void OnStartLocalPlayer()
+        {
+            Debug.Log("OnStartLocalPlayer");
+
+            GetComponent<ThirdPersonUserControl>().enabled = true;
+            //GetComponent<ThirdPersonCharacter>().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+            GetComponentInChildren<AudioListener>().enabled = true;
+        }
+
+    }
+}
